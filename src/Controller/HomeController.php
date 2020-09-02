2547d6f81299edf5e37941bef86b562ff61af917
Danny <ddgame2002@gmail.com>
2020-09-02 17:16:17 +0300

    Add support for en&ru locales

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/", name="home")
+     * @Route("/",name="home")
      */
-    public function logout()
+    public function index(Request $request)
     {
         $user = $this->getUser();
-        return $this->render('home.html.twig', ['user'=>$user]);
+        return $this->render('home.html.twig', ['user' => $user]);
     }
 }
