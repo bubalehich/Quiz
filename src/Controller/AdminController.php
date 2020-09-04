094e6ff6685d89f0d2946621d4aa64020f504e8e
Yana Vlasova <janeridebmx@gmail.com>
2020-09-04 14:46:55 +0300

    Fix deploy issues

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route ("/admin", name="app_admin")
+ * @Route ("/admin")
  * @IsGranted("ROLE_ADMIN")
  */
 class AdminController extends AbstractController
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,6 +37,9 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
