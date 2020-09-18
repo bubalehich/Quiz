4c6a22c244aa58ac70422ce34be4cab460fef41b
Jane <janeridebmx@gmail.com>
2020-09-18 19:27:17 +0300

    Prepare for deploy

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,12 +17,12 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    const USER_COUNT = 1000;
-    const ADMIN_COUNT = 10;
-    const QUIZ_COUNT = 100;
-    const QUESTIONS_COUNT = 1000;
-    const ANSWERS_PER_QUESTION_COUNT = 4;
-    const QUESTIONS_PER_QUIZ_COUNT = 10;
+    const USER_COUNT = 20;
+    const ADMIN_COUNT = 2;
+    const QUIZ_COUNT = 15;
+    const QUESTIONS_COUNT = 75;
+    const ANSWERS_PER_QUESTION_COUNT = 3;
+    const QUESTIONS_PER_QUIZ_COUNT = 5;
 
     private UserPasswordEncoderInterface $encoder;
 
